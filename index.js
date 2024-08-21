6fb8356186f0a8153ebaed3a8e362255487edad2
apekshanakashe123 <nakasheapeksha@gmail.com>
2024-08-21 19:09:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const authRoutes = require('../Backend-Code/routes/LoginRoutes');
-const accountRoutes = require('../Backend-Code/routes/AccountRoutes');
-const TransRoutes = require('../Backend-Code/routes/TransactionRoutes');
+const authRoutes = require('../routes/LoginRoutes');
+const accountRoutes = require('../routes/AccountRoutes');
+const TransRoutes = require('../routes/TransactionRoutes');
 const cors = require('cors');
 const app = express();
 app.use(cors());
