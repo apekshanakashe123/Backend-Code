8f775ff9c7cce4fbe00dd3f56584f86e4dbae0fc
apekshanakashe123 <nakasheapeksha@gmail.com>
2024-08-22 10:06:07 +0530

    Update LoginController.js

--- Controller/LoginController.js
+++ Controller/LoginController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const sql = require('mssql');
 const config = require('../config/database');
 const crypto = require('crypto');
